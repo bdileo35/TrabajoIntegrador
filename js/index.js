d812d769a555804a1d31589678269d6e32fb1a84
Virginia <126522288+virginia1612@users.noreply.github.com>
2023-04-13 14:43:58 -0300

    Update - codigo nosotros

--- js/index.js
+++ js/index.js
@@ -66,6 +66,17 @@ function enviaReclamo(){
 
     
 }
+//Nosotros
+
+const teamMembers = document.querySelectorAll('.team-member');
+for (let i = 0; i < teamMembers.length; i++) {
+  teamMembers[i].addEventListener('mouseover', function() {
+    this.style.boxShadow = '0px 0px 20px rgba(0,0,0,0.6)';
+  });
+  teamMembers[i].addEventListener('mouseout', function() {
+    this.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.2)';
+  });
+}
 
 /*
 
